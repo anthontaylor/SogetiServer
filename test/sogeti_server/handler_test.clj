0e71c83ecd67914e2a16c8c02072b22d7e82e33e
Anthon Taylor <anthonjtaylor@gmail.com>
2017-02-12 21:59:18 -0700

    fixed testing

--- test/sogeti_server/handler_test.clj
+++ test/sogeti_server/handler_test.clj
@@ -1,16 +1,19 @@
 (ns sogeti-server.handler-test
   (:require [clojure.test :refer :all]
+            [ring.util.http-predicates :refer [ok? not-found? bad-request? created?]]
             [ring.mock.request :as mock]
+            [schema-generators.generators :as g]
+            [sogeti-server.schema :as s]
+            [sogeti-server.db :as db]
             [sogeti-server.handler :refer :all]))
 
-(deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
-
-;;Test
+(deftest get-user-test
+  (testing "User application"
+    (let [user (g/generate s/CreateUser)
+          {id :id} (db/insert-user user)]
+      (->
+       (mock/request :get (str "/api/user/" id))
+       app       
+       (as-> {:keys [body] :as response}
+             (and
+              (is (ok? response))))))))
