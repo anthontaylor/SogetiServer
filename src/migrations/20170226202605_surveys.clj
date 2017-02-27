4ed8dbc434438becfab577a4d4236d47bf0a22bd
Anthon Taylor <anthonjtaylor@gmail.com>
2017-02-26 20:34:16 -0700

    added survey table

---
+++ src/migrations/20170226202605_surveys.clj
@@ -0,0 +1,22 @@
+(ns migrations..20170226202605-surveys
+  (:require 	[clojure.java.jdbc :as sql]
+                [sogeti-server.db :as m]))
+
+(defn up
+  "Migrates the database up to version 20170226202605."
+  []
+    (sql/db-do-commands
+   m/db
+   ["CREATE TABLE SURVEYS (
+  		ID BIGINT NOT NULL PRIMARY KEY, 
+       TITLE VARCHAR(255),
+      QUESTION TEXT)"])
+  (println "migrations..20170226202605-surveys up..."))
+
+(defn down
+  "Migrates the database down from version 20170226202605."
+  []
+     (sql/db-do-commands 
+  	m/db
+  	["DROP TABLE SURVEYS"])
+  (println "migrations..20170226202605-surveys down..."))
